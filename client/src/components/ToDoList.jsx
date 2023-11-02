09bb647d9e71a1a1a888c81f23ac58c8ca74bb6f
Erik Lozanov <wolfierecordss@gmail.com>
2023-11-02 13:15:34 +0200

    Added pratice server.

--- client/src/components/ToDoList.jsx
+++ client/src/components/ToDoList.jsx
@@ -7,11 +7,7 @@ export default function ToDoList() {
   </div>
   <div className="table-wrapper">
     {/* Loading spinner - show the load spinner when fetching the data from the server*/}
-    <div className="loading-container">
-      <div className="loading-spinner">
-        <span className="loading-spinner-text">Loading</span>
-      </div>
-    </div>
+
     {/* Todo list table */}
     <table className="table">
       <thead>
